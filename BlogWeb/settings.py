e9ab4fdb974dddc7694e41820fc05c8327cf9db5
akhail <michelbetancourt23@gmail.com>
2016-10-31 00:05:22 -0400

    Remove unnecessary lines, changes language code and time zone

--- BlogWeb/settings.py
+++ BlogWeb/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'django.contrib.humanize',
     'Blog',
+	'taggit',
     'ckeditor'
 ]
 
@@ -106,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Caracas'
 
 USE_I18N = True
 
