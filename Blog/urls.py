b2d0486dc7a84abfced432097b657498b83df62b
akhail <michelbetancourt23@gmail.com>
2016-11-23 22:12:03 -0400

    Last update

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$|inicio', views.index),
-	url(r'^$|esta-pagina', views.details),
-	url(r'^$|entrada/([\w,-]+)', views.details),
-	url(r'^$|nuevo-comentario/', views.details, name='comment')
+    url(r'^$|inicio', views.index),
+    url(r'^$|esta-pagina', views.details),
+    url(r'^$|entrada/([\w,-]+)', views.details),
+    url(r'^$|nuevo-comentario/', views.details, name='comment')
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -9,29 +9,29 @@ from .models import Comment, Post
 
 # Create your views here.
 def index(request):
-	comm = Comment.objects.order_by('-publish_date')[:10]
-	post = Post.objects.order_by('-publish_date')[:10]
-	return render(request, 'Blog/home.html', {'recent_post': post, 'comments': comm})
+    comm = Comment.objects.order_by('-publish_date')[:10]
+    post = Post.objects.order_by('-publish_date')[:10]
+    return render(request, 'Blog/home.html', {'recent_post': post, 'comments': comm})
 
 
 def details(request, article=None):
-	comm = Comment.objects.order_by('-publish_date')[:10]
-	if article is None:
-		return render(request, 'Blog/about.html', {'comments': comm})
-	
-	temp_text = article.replace('-', ' ')
-	ps = Post.objects.get(post_title__iexact=temp_text)
-	if request.POST:
-		form = CommentForm(request.POST)
-		if form.is_valid():
-			comment = form.save(commit=False)
-			comment.publish_date = datetime.datetime.now()
-			comment.post_id = ps
-			comment.save()
-			return HttpResponseRedirect('/entrada/' + article)
-	else:
-		form = CommentForm()
-	
-	comm_post = ps.comment_set.all()
-	return render(request, 'Blog/details.html',
-	              {'article': ps, 'comments_post': comm_post, 'comments': comm, 'form': form})
+    comm = Comment.objects.order_by('-publish_date')[:10]
+    if article is None:
+        return render(request, 'Blog/about.html', {'comments': comm})
+    
+    temp_text = article.replace('-', ' ')
+    ps = Post.objects.get(post_title__iexact=temp_text)
+    if request.POST:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.publish_date = datetime.datetime.now()
+            comment.post_id = ps
+            comment.save()
+            return HttpResponseRedirect('/entrada/' + article)
+    else:
+        form = CommentForm()
+    
+    comm_post = ps.comment_set.all()
+    return render(request, 'Blog/details.html',
+                  {'article': ps, 'comments_post': comm_post, 'comments': comm, 'form': form})
